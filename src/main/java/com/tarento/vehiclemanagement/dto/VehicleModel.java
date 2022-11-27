ed37991dfc33393ddc3200f85bff8adf6a3688e9
Pathik Nandi <pathik.nandi@tarento.com>
2022-11-27 19:23:12 +0530

    new changes

--- src/main/java/com/tarento/vehiclemanagement/dto/VehicleModel.java
+++ src/main/java/com/tarento/vehiclemanagement/dto/VehicleModel.java
@@ -2,20 +2,26 @@ package com.tarento.vehiclemanagement.dto;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
+
 @Entity
 public class VehicleModel extends Auditable{
     @Id
     private long modelId;
     private String modelName;
     private long modelNumber;
+    private String manufacturer;
+    private Date dateOfManufacture;
 
     public VehicleModel() {
     }
 
-    public VehicleModel(long modelId, String modelName, long modelNumber) {
+    public VehicleModel(long modelId, String modelName, long modelNumber, String manufacturer, Date dateOfManufacture) {
         this.modelId = modelId;
         this.modelName = modelName;
         this.modelNumber = modelNumber;
+        this.manufacturer = manufacturer;
+        this.dateOfManufacture = dateOfManufacture;
     }
 
     public long getModelId() {
@@ -42,12 +48,30 @@ public class VehicleModel extends Auditable{
         this.modelNumber = modelNumber;
     }
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Date getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(Date dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
     @Override
     public String toString() {
         return "VehicleModel{" +
                 "modelId=" + modelId +
                 ", modelName='" + modelName + '\'' +
                 ", modelNumber=" + modelNumber +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", dateOfManufacture=" + dateOfManufacture +
                 '}';
     }
 }
