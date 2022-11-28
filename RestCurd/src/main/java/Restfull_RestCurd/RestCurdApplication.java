da11eb16bbb9d3eccc6b8272139631049f6edec2
manokarana <manokaran.anbazhagan@tarento.com>
2022-11-28 11:53:21 +0530

    I create Products , Name , Price

---
+++ RestCurd/src/main/java/Restfull_RestCurd/RestCurdApplication.java
@@ -0,0 +1,12 @@
+package Restfull_RestCurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCurdApplication.class, args);
+	}
+}
