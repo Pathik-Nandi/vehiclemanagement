7f0003fc4ee6366e15278553667716559cfa9df2
Suyash  Gupta <suyash.gupta@tarento.com>
2022-12-07 19:45:49 +0530

    change

--- demo (3)/demo/src/main/java/com/tarento/demo/data/VehicleSpecDao.java
+++
@@ -1,9 +0,0 @@
-package com.tarento.demo.data;
-
-import com.tarento.demo.dto.VehicleSpec;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-    public interface VehicleSpecDao extends JpaRepository<VehicleSpec,Long> {
-    }
