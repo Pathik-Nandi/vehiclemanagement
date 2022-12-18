1da33059f2bd2e316c8ae2f169e49dbd1ca50cd5
Suyash Gupta <suyash2000.sg@gmail.com>
2022-12-18 20:37:42 +0530

    add @timed

--- src/main/java/com/tarento/vehiclemanagement/vehiclespecification/controller/VehicleSpecController.java
+++ src/main/java/com/tarento/vehiclemanagement/vehiclespecification/controller/VehicleSpecController.java
@@ -3,6 +3,7 @@ package com.tarento.vehiclemanagement.vehiclespecification.controller;
 import com.tarento.vehiclemanagement.apiresponse.APIResponse;
 import com.tarento.vehiclemanagement.vehiclespecification.dto.VehicleSpec;
 import com.tarento.vehiclemanagement.vehiclespecification.service.VehicleSpecService;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,7 @@ public class VehicleSpecController {
         return new APIResponse(true, "200", modelId);
     }
     @GetMapping("/findByModelId/{modelId}")
+    @Timed(value = "add vehicleSpec", description = "time taken for adding vehicleSpec")
     public VehicleSpec findBymodelId(@PathVariable long ModelId) {
         return vehicleSpecService.findVehicleSpecBymodelId(ModelId);
     }
